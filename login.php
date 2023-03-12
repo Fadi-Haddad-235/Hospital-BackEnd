9e9bc9d95496d9ffcdda016c89e7e223c733f93f
Fadi Haddad <fadi.haddad.235@gmail.com>
2023-03-12 16:46:56 +0200

    added JWT authetication

--- login.php
+++ login.php
@@ -1,4 +1,9 @@
 <?php
+
+require_once 'vendor/autoload.php';
+use Firebase\JWT\JWT;
+$secret_key = 'your_secret_key';
+
 include('connection.php');
 
 $email = $_POST['email'];
@@ -14,15 +19,28 @@ $num_rows = $query->num_rows();
 $query->bind_result($id,$username,$email, $hashed_password,$DOB, $usertype);
 $query->fetch();
 $response = [];
-if ($num_rows == 5) {
+if ($num_rows == 0) {
     $response['response'] = "user not found";
 } else {
     if (password_verify($password, $hashed_password)) {
-        $response['response'] = "logged in";
-        $response['user_id'] = $id;
-        $response['username'] = $username;
-        $response['usertype'] = $usertype;
-
+        // $response['response'] = "logged in";
+        // $response['user_id'] = $id;
+        // $response['username'] = $username;
+        // $response['usertype'] = $usertype;
+        $payload = array(
+            "user_id" => $id,
+            "username" => $username,
+            "usertype" => $usertype
+        );
+        $jwt = JWT::encode($payload, $secret_key,'HS256');
+        $response = array(
+            "response" => "logged in",
+            "user_id" => $id,
+            "username" => $username,
+            "usertype" => $usertype,
+            "jwt" => $jwt
+        );
+        
     } else {
         $response["response"] = "Incorrect email/password";
     }
